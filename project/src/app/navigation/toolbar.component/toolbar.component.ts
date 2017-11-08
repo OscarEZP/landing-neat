a6d139742a87d2ffcad42bfaa447d08781b0f28d
Alejandro Cornejo <acornejs@everis.com>
2017-11-08 16:32:53 -0300

    Main Toolbar Service Get UTC-Back Done

--- project/src/app/navigation/toolbar.component/toolbar.component.ts
+++ project/src/app/navigation/toolbar.component/toolbar.component.ts
@@ -1,25 +1,50 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
-import {SidenavComponent} from '../sidenav.component/sidenav.component';
-import {Subscription} from 'rxjs/Subscription';
+import {OnDestroy, AfterContentInit, Component, OnInit} from '@angular/core';
+import {DatetimeService} from "../../commons/datatime.service/datetime.service";
+import {ActualTimeModel} from "../../commons/datatime.service/actual-time-model";
+import {TimerObservable} from "rxjs/observable/TimerObservable";
+import { Observable } from "rxjs";
 
 @Component({
     selector: 'lsl-toolbar',
     templateUrl: './toolbar.component.html',
     styleUrls: ['./toolbar.component.css']
 })
-export class ToolbarComponent implements AfterViewInit {
-    @ViewChild('mySidenav')
-    sidenav: SidenavComponent;
+export class ToolbarComponent implements OnInit, AfterContentInit, OnDestroy {
 
-    susbscription: Subscription;
+    private data: ActualTimeModel;
+    private display: boolean;
+    private alive: boolean;
+    private interval: number;
+    public currentDate: number;
 
-    ngAfterViewInit(): void {
-        this.susbscription = this.sidenav.onClose.subscribe(() =>
-            console.log('Closed event from observable'));
+    constructor(private datetimeService:DatetimeService) {
+        this.display = false;
+        this.alive = true;
+        this.interval = 500000;
+        this.currentDate = 0;
     }
 
-    onClose(): void {
-        console.log('Closed event from template');
+    ngOnInit() {
+        this.currentDate = 0;
+    }
+
+    ngAfterContentInit() {
+        TimerObservable.create(0, this.interval)
+            //.takeWhile(() => this.alive)
+            .subscribe(() => {
+                this.datetimeService.getTime()
+                    .subscribe((data) => {
+                        this.data = data;
+                        this.currentDate = this.data.currentTimeLong;
+                        if(!this.display){
+                            this.display = true;
+                        }
+                    });
+            });
+    }
+
+    ngOnDestroy() {
+        this.alive = false;
     }
 
 }
