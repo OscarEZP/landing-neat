04234a0e0d8568008832fad15a6081ab30df70ea
Marcela Illanes <millanes@everis.com>
2017-11-08 13:36:22 -0300

    Add Style | Side Nav Component

--- project/src/app/navigation/sidenav.component/sidenav.component.ts
+++ project/src/app/navigation/sidenav.component/sidenav.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'lsl-sidenav',
-  templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+    selector: 'lsl-sidenav',
+    templateUrl: './sidenav.component.html',
+    styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
+    private username: String;
+    private mail: String;
+    private arrMenu: {label:String,link:String}[];
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.username = "Ignacio Pardo";
+        this.mail = "ignacio.pardo@gmail.com";
+        this.arrMenu = [{"label" :"Dashboard","link":""},{"label":"Operations Module","link":""}];
+
+    }
 
 }
