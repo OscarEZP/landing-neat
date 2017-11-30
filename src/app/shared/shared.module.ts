0c6ab52c6a42ee91043584307cca8a2c5ad7fe06
pablo <pbaltier@everis.com>
2017-11-30 09:47:46 -0300

    Mocking data

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,7 +5,7 @@ import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
     declarations: [
-        CountdownComponent
+        CountdownComponent,
     ],
     imports: [
         MaterialModule,
