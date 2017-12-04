45ef391b1a9fcce1d11b457c3d4647a9abb30691
Alejandro Cornejo <acornejs@everis.com>
2017-12-04 12:46:01 -0300

    Environment names changed

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,17 +1,18 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost',
+    envName: 'staging',
+    apiUrl: 'https://staging.moc.maintenix.aero',
     paths: {
-        confirmForgotPassword: ':9001/api/security/users/_confirmforgotpassword',
-        contingencyList: ':9002/api/contingencies',
-        dateTime: ':9001/api/security/currentdatetime',
-        forgotPassword: ':9001/api/security/users/_forgotpassword',
-        login: ':9001/api/security/users/_login',
-        safetyEvent: ':9002/api/contingencies/configurations/safetyEvents',
-        aircrafts: ':9002/api/contingencies/configurations/aircrafts',
-        flights: ':9002/api/contingencies/configurations/flights',
-        locations: ':9002/api/contingencies/configurations/locations',
-        types: ':9002/api/contingencies/configurations/types',
-        close: ':9002/api/contingencies/_close'
+        confirmForgotPassword: '/api/security/users/_confirmforgotpassword',
+        contingencyList: '/api/contingencies',
+        dateTime: '/api/security/currentdatetime',
+        forgotPassword: '/api/security/users/_forgotpassword',
+        login: '/api/security/users/_login',
+        safetyEvent: '/api/contingencies/configurations/safetyEvents',
+        aircrafts: '/api/contingencies/configurations/aircrafts',
+        flights: '/api/contingencies/configurations/flights',
+        locations: '/api/contingencies/configurations/locations',
+        types: '/api/contingencies/configurations/types',
+        close: '/api/contingencies/_close'
     }
 };
