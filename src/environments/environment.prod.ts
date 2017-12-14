6942e6a95b67c5af624b872f27bd13e06398b15b
Gabo <gabriel.moraga02@gmail.com>
2017-12-14 16:24:41 -0300

    add configuration production

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+export const environment = {
+    production: true,
+    envName: 'production',
+    apiUrl: 'https://moc.maintenix.aero',
+    paths: {
+        confirmForgotPassword: '/api/security/users/_confirmforgotpassword',
+        contingencyList: '/api/contingencies',
+        dateTime: '/api/security/currentdatetime',
+        forgotPassword: '/api/security/users/_forgotpassword',
+        login: '/api/security/users/_login',
+        safetyEvent: '/api/contingencies/configurations/safetyEvents',
+        aircrafts: '/api/contingencies/configurations/aircrafts',
+        flights: '/api/contingencies/configurations/flights',
+        locations: '/api/contingencies/configurations/locations',
+        types: '/api/contingencies/configurations/types',
+        close: '/api/contingencies/_close'
+    }
+};
