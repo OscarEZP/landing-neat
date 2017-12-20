0b1a57be571aeb5f90bfda6ce1096eed743f3504
Gabo <gabriel.moraga02@gmail.com>
2017-12-20 09:27:04 -0300

    add configuration environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
-export const config = {
-  production: true
+export const environment = {
+    production: true,
+    envName: 'prod',
+    apiUrl: 'https://mcp.maintenix.ifs.cloud',
+    paths: {
+        confirmForgotPassword: '/api/security/users/_confirmforgotpassword',
+        contingencyList: '/api/v1/contingencies',
+        dateTime: '/api/security/currentdatetime',
+        forgotPassword: '/api/security/users/_forgotpassword',
+        login: '/api/security/users/_login',
+        safetyEvent: '/api/v1/contingencies/configurations/safetyEvents',
+        aircrafts: '/api/v1/contingencies/configurations/aircrafts',
+        flights: '/api/v1/contingencies/configurations/flights',
+        locations: '/api/v1/contingencies/configurations/locations',
+        types: '/api/v1/contingencies/configurations/types',
+        close: '/api/v1/contingencies/_close',
+        followUp: '/api/v1/contingencies/_followUp',
+    }
 };
