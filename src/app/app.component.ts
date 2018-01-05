7904aac2b00af0672ddb3eee5b66780473e0df12
pablo <pbaltier@everis.com>
2018-01-05 08:06:45 -0300

    Hotjar config updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {environment} from '../environments/environment';
 
 @Component({
     selector: 'lsl-root',
@@ -7,5 +8,27 @@ import {Component} from '@angular/core';
 
 })
 export class AppComponent {
-}
+    constructor() {
+        if (environment.hotjarConfig.enabled) {
+            this.initHotJar();
+        }
+    }
 
+    public initHotJar() {
+        const h = window;
+        const o = document;
+        const t = `//static.hotjar.com/c/hotjar-`;
+        const j = `.js?sv=`;
+        let a;
+        let r;
+        h['hj'] = h['hj'] || function () {
+                (h['hj'].q = h['hj'].q || []).push(arguments);
+            };
+        h['_hjSettings'] = {hjid: environment.hotjarConfig.id, hjsv: 6};
+        a = o.getElementsByTagName('head')[0];
+        r = o.createElement('script');
+        r.async = 1;
+        r.src = t + h['_hjSettings'].hjid + j + h['_hjSettings'].hjsv;
+        a.appendChild(r);
+    }
+}
