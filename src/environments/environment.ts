0fede7889dc99cd7ea5196345f0b9d88a7ec771f
millanes <marcela.illanes.benavides@everis.com>
2018-01-10 00:25:55 -0300

    RF-Change Paht Configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,23 +3,23 @@ export const environment = {
     envName: 'staging',
     apiUrl: 'https://staging.mcp.maintenix.ifs.cloud',
     paths: {
-        aircrafts: '/api/v1/contingencies/configurations/aircrafts',
-        aircraftsSearch: '/api/v1/contingencies/configurations/aircrafts/_search',
+        aircrafts: '/api/v1/configurations/aircrafts',
+        aircraftsSearch: '/api/v1/configurations/aircrafts/_search',
         close: '/api/v1/contingencies/_close',
-        configMaxStatus: '/api/v1/contingencies/configurations/status/max',
-        configStatus: '/api/v1/contingencies/configurations/status/_search',
+        configMaxStatus: '/api/v1/configurations/status/max',
+        configStatus: '/api/v1/configurations/status/_search',
         confirmForgotPassword: '/api/security/users/_confirmforgotpassword',
         contingencyList: '/api/v1/contingencies',
         contingencySearch: '/api/v1/contingencies/_search',
         dateTime: '/api/security/currentdatetime',
-        flights: '/api/v1/contingencies/configurations/flights/_search',
+        flights: '/api/v1/configurations/flights/_search',
         followUp: '/api/v1/contingencies/status/_followUp',
         forgotPassword: '/api/security/users/_forgotpassword',
-        locations: '/api/v1/contingencies/configurations/locations',
+        locations: '/api/v1/configurations/locations',
         login: '/api/security/users/_login',
-        operator: '/api/v1/contingencies/configurations/operators/',
-        safetyEvent: '/api/v1/contingencies/configurations/safetyEvents',
-        types: '/api/v1/contingencies/configurations/types',
-        closeType: '/api/v1/contingencies/configurations/types/groupNames'
+        operator: '/api/v1/configurations/operators/',
+        safetyEvent: '/api/v1/configurations/safetyEvents',
+        types: '/api/v1/configurations/types',
+        closeType: '/api/v1/configurations/types/groupNames'
     }
 };
