1a66a3e42b181f24119b7fb122ec8d4e9565ce67
Pablo Baltierra <pbaltier@everis.com>
2018-03-26 12:29:35 -0300

    364 | 566 Apply / Don't apply feature

--- src/app/content/fleet-health/historical-report/timeline-tooltip/timeline-tooltip.component.ts
+++ src/app/content/fleet-health/historical-report/timeline-tooltip/timeline-tooltip.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, Input, Output, OnDestroy, OnInit} from '@angular/core';
-import {Task} from '../../../../shared/_models/task/task';
 import {Review} from '../../../../shared/_models/task/analysis/review';
+import {Task} from '../../../../shared/_models/task/task';
+import {TimelineTask} from '../../../../shared/_models/task/timelineTask';
 
 @Component({
     selector: 'lsl-tooltip',
@@ -10,34 +11,37 @@ import {Review} from '../../../../shared/_models/task/analysis/review';
 export class TimelineTooltipComponent implements OnInit, OnDestroy {
 
     @Input()
-    public task: Task;
+    public timelineTaskData: object;
 
     @Output()
     public onApply: EventEmitter<Review> = new EventEmitter();
 
     private _review: Review;
+    private _timelineTask: TimelineTask;
+    private _task: Task;
 
     constructor() {
         this._review = Review.getInstance();
     }
 
     ngOnInit(): void {
+        this.timelineTask = this.timelineTaskData['data'];
+        this.task = this.timelineTask.task;
+        if (this.timelineTask.apply !== null) {
+            this.review.status = this.timelineTask.apply ? 'apply' : 'dontApply';
+        }
     }
 
     ngOnDestroy(): void {
     }
 
     public checkStatus() {
-        this.review.barcode = this.task.barcode;
+        this.review.barcode = this.timelineTaskData.data.task.barcode;
         this.onApply.emit(this.review);
     }
 
     get review(): Review {
         return this._review;
-    }
-
-    set review(value: Review) {
-        this._review = value;
     }
 
     get status(): string {
@@ -48,4 +52,19 @@ export class TimelineTooltipComponent implements OnInit, OnDestroy {
         this._review.status = value;
     }
 
+    get timelineTask(): TimelineTask {
+        return this._timelineTask;
+    }
+
+    set timelineTask(value: TimelineTask) {
+        this._timelineTask = value;
+    }
+
+    get task(): Task {
+        return this._task;
+    }
+
+    set task(value: Task) {
+        this._task = value;
+    }
 }
