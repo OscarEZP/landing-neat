c189c1b7de3e14c436b284170c951b32618a9df2
Pablo Baltierra <pbaltier@everis.com>
2018-04-27 10:17:07 -0300

    MOC-622 | 689 Port fix

--- src/app/content/management/management.module.ts
+++ src/app/content/management/management.module.ts
@@ -4,6 +4,7 @@ import {ManagementComponent} from './management.component';
 import {SharedModule} from '../../shared/shared.module';
 import {RouterModule} from '@angular/router';
 import {UserManagementModule} from './mng-general/user-management/user-management.module';
+import { UserListComponent } from './mng-general/user-list/user-list.component';
 
 @NgModule({
     imports: [
@@ -13,7 +14,8 @@ import {UserManagementModule} from './mng-general/user-management/user-managemen
         UserManagementModule
     ],
     declarations: [
-        ManagementComponent
+        ManagementComponent,
+        UserListComponent
     ]
 })
 export class ManagementModule {
