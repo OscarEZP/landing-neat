827e593654ffa4a0c58f65c03413f004cf5dd0a9
Pablo Baltierra <pbaltier@everis.com>
2018-06-20 09:41:52 -0400

    Merge branch 'MOC-592-EditHistoricalReport' of https://bitbucket.org/mxitechnologies/moc-web into moc-835-timeline-with-historical-reports
    # Conflicts:
    #	src/app/content/fleet-health/historical-report/timeline-report/timeline-report.component.ts

--- src/app/content/fleet-health/historical-report/timeline-report/timeline-report.component.ts
+++ src/app/content/fleet-health/historical-report/timeline-report/timeline-report.component.ts
@@ -34,6 +34,7 @@ export class TimelineReportComponent implements OnInit, OnDestroy {
     private static TASK_SEARCH_ENDPOINT = 'taskRelationsSearch';
     private static TASK_HISTORICAL_ENDPOINT = 'taskHistorical';
     private static REPORT_CLOSE = 'CLOSE';
+    private static TIMELINE_DATE_FORMAT = 'YYYY-MM-DD';
 
     @Output()
     onAnalyzedTaskSelected: EventEmitter<TimelineTask> = new EventEmitter();
