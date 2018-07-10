2a871153c840804a39d70b76026611b1279a8a44
rodrigo <rbustaac@everis.com>
2018-07-10 14:29:54 -0400

    MOC-876|949 Model AOG

--- src/app/content/operations/aog-list/aog-list.component.ts
+++ src/app/content/operations/aog-list/aog-list.component.ts
@@ -3,6 +3,8 @@ import {Aog} from '../../../shared/_models/aog/aog';
 import {TranslateService} from '@ngx-translate/core';
 import {LayoutService} from '../../../layout/_services/layout.service';
 import {TimeInstant} from '../../../shared/_models/timeInstant';
+import {StatusAog} from '../../../shared/_models/aog/statusAog';
+import {Interval} from '../../../shared/_models/interval';
 
 @Component({
   selector: 'lsl-aog-list',
@@ -32,33 +34,34 @@ export class AogListComponent implements OnInit {
     this.error = false;
 
     this.aogList = new Array(2);
-    const aogEtr: Aog = new Aog();
-    const aogNi: Aog = new Aog();
+    const aogEtr: Aog = Aog.getInstance();
+    const aogNi: Aog = Aog.getInstance();
 
     aogEtr.tail = 'PT-MZU';
     aogEtr.fleet = 'A320';
     aogEtr.operator = 'LA';
     aogEtr.barcode = 'TOO3LUTQ';
     aogEtr.reason = 'HOUVE COLISÃO DA ACFT COM FINGER, AFETANDO A PARTE SUPERIOR DO PYLON';
-    aogEtr.status = 'ETR';
-    aogEtr.isSafetyEvent = true;
-    aogEtr.openAogDate = new TimeInstant(Date.now(), null);
-    aogEtr.openStatusDate = new TimeInstant(Date.now(), null);
-    aogEtr.durationAog = 1800000;
-    aogEtr.durationStatus = 1800000;
+    aogEtr.safety = 'ABT';
+
+    const statusETR = new StatusAog(null, null, null, null, null, null, null);
+    statusETR.code = 'ETR';
+    statusETR.creationDate = new TimeInstant(Date.now(), null);
+    statusETR.requestedInterval = new Interval( new TimeInstant(Date.now(), null), 1800000);
+    aogEtr.status = statusETR;
+
 
     aogNi.tail = 'PT-MZU';
     aogNi.fleet = 'A320';
     aogNi.operator = 'LA';
     aogNi.barcode = 'TOO3LUTT';
     aogNi.reason = 'HOUVE COLISÃO DA ACFT COM FINGER, AFETANDO A PARTE SUPERIOR DO PYLON';
-    aogNi.status = 'NI';
-    aogNi.isSafetyEvent = false;
-    aogNi.openAogDate = new TimeInstant(Date.now(), null);
-    aogNi.openStatusDate = new TimeInstant(Date.now(), null);
-    aogNi.durationAog = 1800000;
-    aogNi.durationStatus = 1800000;
 
+    const statusNi = new StatusAog(null, null, null, null, null, null, null);
+    statusNi.code = 'NI';
+    statusNi.creationDate = new TimeInstant(Date.now(), null);
+    statusNi.requestedInterval = new Interval( new TimeInstant(Date.now(), null), 1800000);
+    aogNi.status = statusNi;
 
     this.aogList[0] = aogEtr;
     this.aogList[1] = aogNi;
---
+++ src/app/shared/_models/aog/statusAog.ts
@@ -0,0 +1,83 @@
+import {TimeInstant} from '../timeInstant';
+import {Interval} from '../interval';
+export class StatusAog {
+
+    private _aogId: number;
+    private _code: string;
+    private _observation: string;
+    private _requestedInterval: Interval;
+    private _realInterval: Interval;
+    private _username: string;
+    private _creationDate: TimeInstant;
+
+    constructor(code: string, aogId: number, creationDate: TimeInstant, observation: string, realInterval: Interval, requestedInterval: Interval, username: string) {
+        this._code = code;
+        this._aogId = aogId;
+        this._creationDate = creationDate != null ? creationDate : TimeInstant.getInstance();
+        this._observation = observation;
+        this._realInterval = realInterval != null ? realInterval : Interval.getInstance();
+        this._requestedInterval = requestedInterval != null ? requestedInterval : Interval.getInstance();
+        this._username = username;
+    }
+
+    static getInstance(): StatusAog {
+        return new StatusAog(null, null, null, null, null, null, null);
+    }
+
+    get aogId(): number {
+        return this._aogId;
+    }
+
+    set aogId(value: number) {
+        this._aogId = value;
+    }
+
+    get code(): string {
+        return this._code;
+    }
+
+    set code(value: string) {
+        this._code = value;
+    }
+
+    get observation(): string {
+        return this._observation;
+    }
+
+    set observation(value: string) {
+        this._observation = value;
+    }
+
+    get requestedInterval(): Interval {
+        return this._requestedInterval;
+    }
+
+    set requestedInterval(value: Interval) {
+        this._requestedInterval = value;
+    }
+
+    get realInterval(): Interval {
+        return this._realInterval;
+    }
+
+    set realInterval(value: Interval) {
+        this._realInterval = value;
+    }
+
+    get username(): string {
+        return this._username;
+    }
+
+    set username(value: string) {
+        this._username = value;
+    }
+
+    get creationDate(): TimeInstant {
+        return this._creationDate;
+    }
+
+    set creationDate(value: TimeInstant) {
+        this._creationDate = value;
+    }
+}
+
